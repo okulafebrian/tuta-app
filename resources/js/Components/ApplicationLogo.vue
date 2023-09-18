0cac5c81efbeb60811b1dcbe79b1bc9168e9bd05
okulafebrian <okulafebrian@gmail.com>
2023-09-18 14:03:46 +0700

    new app

---
+++ resources/js/Components/ApplicationLogo.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="/storage/assets/logo.png" alt="">
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia>{{ config('app.name', 'TUTA') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/storage/assets/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/storage/assets/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/storage/assets/favicon/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+
+        <!-- Scripts -->
+        @routes
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased bg-lime-50">
+        @inertia
+    </body>
+</html>
